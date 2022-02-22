797c75f6feb5e457face194336d1e8a078d2c144
Renan Henrique <henriquebasshvf@gmail.com>
2022-02-22 10:51:29 -0300

    update: readme

--- dist/coverFlow.js
+++ dist/coverFlow.js
@@ -5,8 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
-require("./coverFlow.css");
-
 var _react = _interopRequireDefault(require("react"));
 
 var _reactSwipeable = require("react-swipeable");
